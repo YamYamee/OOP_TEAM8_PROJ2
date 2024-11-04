416bdd246c9e40930f5511c419287e19ce0c81d0
dev_jaeho <33758013+jaeho0718@users.noreply.github.com>
2024-11-05 08:57:48 +0900

    🔨 테스트 코드 수정

--- PROJ2/main.cpp
+++ PROJ2/main.cpp
@@ -2,7 +2,6 @@
 #include <iostream>
 #include <string.h>
 #include <stdlib.h>
-#include <stdlib.h>
 
 using namespace std;
 
@@ -12,6 +11,9 @@ enum OperatorType {
 
 void test_operator(inf_int a1, inf_int a2, inf_int t, OperatorType type) 
 {
+	static int count = 0;
+	count++;
+	cout<<"("<<count<<") ";
 	bool is_success = false;
 	inf_int result;
 	switch (type)
@@ -66,8 +68,8 @@ int main()
 {
 	// 한자리수
 	test_operator(1, 1, 0, EQUAL);
-	test_operator(1, 2, 0, NOTEQUAL);
-	test_operator(1, 2, 0, SMALLER);
+	test_operator(1, -2, 0, NOTEQUAL);
+	test_operator(-2, 1, 0, SMALLER);
 	test_operator(3, 1, 0, LARGER);
 	test_operator(1, 2, 3, ADD);
 	test_operator(2, 1, 1, SUBTRACT);
@@ -96,7 +98,7 @@ int main()
 	test_operator(inf_int("112311231231231"), inf_int("112351231231231"), 0, SMALLER);
 	test_operator(inf_int("112351231231231"), inf_int("112311231231231"), 0, LARGER);
 	test_operator(inf_int("112351231231231"), inf_int("112311231231231"), inf_int("224662462462462"), ADD);
-	test_operator(inf_int("224662462462465"), inf_int("12132931302198"), inf_int("212529531160267"), SUBTRACT);
+	test_operator(inf_int("12012"), inf_int("12334"), inf_int("-322"), SUBTRACT);
 	test_operator(inf_int("12132931302198"), inf_int("224662462462465"), inf_int("-212529531160267"), SUBTRACT);
 	test_operator(inf_int("12132931302198"), inf_int("12345"), inf_int("149781036925634310"), MULTIPLY);
 	test_operator(inf_int("12132931302198"), inf_int("-12345"), inf_int("-149781036925634310"), MULTIPLY);
